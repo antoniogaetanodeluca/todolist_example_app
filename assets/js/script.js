79ea8544ff8e0a4c473fa178e5c9ae642c4ddd5b
Antonio Gaetano De Luca <antoniogaetanodeluca@antoniodeluca.local>
2017-04-22 20:02:51 +0200

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,76 @@
+var ToDoList = function(){
+
+  var toDo = [
+    {
+      id:"1234",
+      title:"first toDo",
+      date: "23 December",
+      priority: "si"
+    },
+    {
+      id:"4567",
+      title:"second toDo",
+      date: "23 March",
+      priority: "no"
+    }
+  ];
+
+  return {
+    getToDo: function(){
+      return toDo;
+    },
+
+    addToDo: function(todo){
+      toDo.push(todo);
+    }
+  }
+
+};
+
 document.addEventListener("DOMContentLoaded", function(event) {
 
-  var ToDoList = function(){
-
-    var toDo = [
-      {
-        id:"1234",
-        titolo:"first toDo",
-        data: "23 December",
-        urgenza: "si"
-      },
-      {
-        id:"4567",
-        titolo:"second toDo",
-        data: "23 March",
-        urgenza: "no"
-      },
-
-    ];
-
-    return {
-      getToDo: function(){
-        return toDo;
-      },
-
-      addToDo: function(todo){
-        toDo.push(todo);
-      }
-    }
-
-  };
-
   var myApp = ToDoList();
+  var doc = document;
 
-  myApp.addToDo({
-    id:"5656",
-    titolo:"third toDo",
-    data: "29 March",
-    urgenza: "no"}
-  );
-  console.log(myApp.getToDo());
+  var input_todo = doc.querySelector("#input_todo");
+  input_todo.addEventListener("keypress", function(e){
+    if(e.keyCode === 13 && e.target.value != "" && e.target.value.length >= 4){
+      var objToDo_to_add = {
+        id:"000",
+        title: e.target.value,
+        date: "23 December",
+        priority: "si"
+      }
+      myApp.addToDo(objToDo_to_add);
+    }
+  });
 
+  var ul_todolist = doc.querySelector("#todolist");
+  var toDoObj = myApp.getToDo();
+  // FOR METHOD to read the todo list
+  // for(var i=0; i<toDoObj.length; i++){
+  //   var li, li_element, li_node, todo_title, todo_date, todo_priority;
+  //   todo_title = toDoObj[i].title;
+  //   todo_date = toDoObj[i].date;
+  //   todo_priority = toDoObj[i].priority;
+  //   li = doc.createElement("li");
+  //   li_element = todo_date + " - " + todo_title + " - " + todo_priority;
+  //   li_node = doc.createTextNode(li_element);
+  //   li.appendChild(li_node);
+  //   ul_todolist.appendChild(li);
+  // }
+
+  //FOREACH METHOD to read the todo list
+  toDoObj.forEach(function(todoelement){
+    var li, li_element, li_node, todo_title, todo_date, todo_priority;
+    todo_title = todoelement.title;
+    todo_date = todoelement.date;
+    todo_priority = todoelement.priority;
+    li = doc.createElement("li");
+    li_element = todo_date + " - " + todo_title + " - " + todo_priority;
+    li_node = doc.createTextNode(li_element);
+    li.appendChild(li_node);
+    ul_todolist.appendChild(li);
+  });
 
 });
