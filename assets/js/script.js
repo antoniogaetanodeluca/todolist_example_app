fcc8bc7d89d0dbf0e0e58f4faf6ec0c277d06d7a
Antonio Gaetano De Luca <antoniogaetanodeluca@antoniodeluca.local>
2017-04-25 13:02:29 +0200

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ var toDo = function(){
 
   return {
     getToDo: function(){
-      
+      return toDoObject;
     },
     addToDo: function(todo){
       toDoObject.push(todo);
@@ -36,7 +36,18 @@ document.addEventListener("DOMContentLoaded", function(e){
 
   var myApp = new toDo;
 
-  myApp.getToDo();
-  //myApp.addToDo(todo);
+  var obj = myApp.getToDo();
+  var li, li_title, li_year, li_description, li_node, li_content;
+  var ul_list = document.getElementById("todo_list");
+  obj.forEach(function (k){
+    li = document.createElement("li");
+    li_title = k.title;
+    li_description = k.description;
+    li_year = k.date[2];
+    li_content = "<div class='toto_item'><span class='date'>"+li_year+"</span> - <span class='title'>"+li_title+"<span class='description'>"+li_description+"</span></div>";
+    li_node = document.createTextNode(li_content);
+    li.appendChild(li_node);
+  });
+  ul_list.appendChild(li);
 
 });
