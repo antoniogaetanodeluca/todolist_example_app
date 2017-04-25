cb242d85d087a14226108dec81affb12ac56ab87
Antonio Gaetano De Luca <antoniogaetanodeluca@antoniodeluca.local>
2017-04-25 19:22:25 +0200

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@ var toDo = function(){
       year = data.getFullYear();
       day = data.getDay();
       month = data.getMonth();
-      id = Math.random() * 100;
 
   var toDoObject = [
     {
@@ -27,12 +26,7 @@ var toDo = function(){
       return toDoObject;
     },
     addToDo: function(title_todo, description_todo){
-      var data, year, day, month, id;
-          data = new Date();
-          year = data.getFullYear();
-          day = data.getDay();
-          month = data.getMonth();
-          id = Math.random() * 100;
+      id = Math.floor(Math.random() * 100);
       var todo = {
         id: id,
         title: title_todo,
@@ -51,25 +45,39 @@ document.addEventListener("DOMContentLoaded", function(e){
 
   var li, li_title, li_year, li_description, li_node, li_content;
   var ul_list = document.getElementById("todo_list");
-
-  var title_todo = document.getElementById("title_todo").value;
-  var description_todo = document.getElementById("description_todo").value;
   var submit_todo = document.getElementById("submit_todo");
-  submit_todo.addEventListener("click", myApp.addToDo(title_todo, description_todo), false);
-  console.log(myApp.getToDoList());
 
-  var obj = myApp.getToDoList();
-  obj.forEach(function (k){
-    li = document.createElement("li");
-    li_title = k.title;
-    li_description = k.description;
-    li_year = k.date[2];
-    li_content = "<div class='todo_item'><span class='title'><h2>"+li_title+"</h2></span><span class='date'>"+li_year+"</span><span class='description'><p>"+li_description+"</p></span></div>";
-    li.innerHTML = li_content;
-    ul_list.appendChild(li);
+
+  submit_todo.addEventListener("click", function(e){
+    e.preventDefault();
+    var title_todo = document.getElementById("title_todo").value;
+    var description_todo = document.getElementById("description_todo").value;
+
+    if( (title_todo.length >= 4) && (description_todo.length >= 5)){
+      myApp.addToDo(title_todo, description_todo);
+      title_todo.value = "";
+      description_todo.value = "";
+    } else {
+      title_todo
+    }
+
+    console.log(myApp.getToDoList());
+
   });
 
 
+  // var obj = myApp.getToDoList();
+  // obj.forEach(function (k){
+  //   li = document.createElement("li");
+  //   li_title = k.title;
+  //   li_description = k.description;
+  //   li_year = k.date[2];
+  //   li_content = "<div class='todo_item'><span class='title'><h2>"+li_title+"</h2></span><span class='date'>"+li_year+"</span><span class='description'><p>"+li_description+"</p></span></div>";
+  //   li.innerHTML = li_content;
+  //   ul_list.appendChild(li);
+  // });
+  //
+  // console.log(myApp.getToDoList());
 
 
 });
