83eb96fd163651a24543833502dfdeeda4e15e60
Antonio Gaetano De Luca <antoniogaetanodeluca@antoniodeluca.local>
2017-04-26 17:10:50 +0200

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,20 +11,46 @@ var toDo = function(){
       id: "123",
       title: "First To Do",
       description: "Lorem Ipsum dolor sit amet",
-      date: [day, month, year]
+      date: [day, month, year],
+      completed: false
     },
     {
       id: "456",
       title: "Second To Do",
       description: "Lorem Ipsum dolor sit amet 2",
-      date: [day, month, year]
+      date: [day, month, year],
+      completed: false
     }
   ]
+
+  var requiredField = function(field, className){
+    if(field.classList.contains(className)){
+      field.classList.remove(className);
+    } else {
+      field.classList.add(className);
+    }
+  }
+
+  var showToDoList = function(){
+    var ul_list = document.getElementById("todo_list");
+    ul_list.innerHTML = "";
+    var obj = toDoObject;
+    obj.forEach(function (k){
+      li = document.createElement("li");
+      li_title = k.title;
+      li_description = k.description;
+      li_year = k.date[2];
+      li_content = "<div class='todo_item'><span class='title'><h2>"+li_title+"</h2></span><span class='date'>"+li_year+"</span><span class='description'><p>"+li_description+"</p></span><button class='delete_todo btn btn-danger'>done</button><button class='restore btn btn-warning hidden'>restore</button></div>";
+      li.innerHTML = li_content;
+      ul_list.appendChild(li);
+    })
+  }
 
   return {
     getToDoList: function(){
       return toDoObject;
     },
+
     addToDo: function(title_todo, description_todo){
       id = Math.floor(Math.random() * 100);
       var todo = {
@@ -34,23 +60,22 @@ var toDo = function(){
         date: [day, month, year]
       }
       toDoObject.push(todo);
-    }
+    },
+
+    showToDoList: showToDoList
   }
 
 }
 
 document.addEventListener("DOMContentLoaded", function(e){
 
-  function addClass(el, className){
-
-  }
-
   var myApp = new toDo;
 
-  var li, li_title, li_year, li_description, li_node, li_content;
-  var ul_list = document.getElementById("todo_list");
-  var submit_todo = document.getElementById("submit_todo");
+  myApp.showToDoList();
 
+  var li, li_title, li_year, li_description, li_node, li_content;
+  var submit_todo = document.getElementById("submit_todo");
+  var delete_todo = document.getElementsByClassName("delete_todo");
 
   submit_todo.addEventListener("click", function(e){
     e.preventDefault();
@@ -58,30 +83,22 @@ document.addEventListener("DOMContentLoaded", function(e){
     var description_todo = document.getElementById("description_todo").value;
     var form_todo = document.getElementById("form_todo");
 
-    if( (title_todo.length >= 4) && (description_todo.length >= 5)){
-      myApp.addToDo(title_todo, description_todo);
-      
+    if( (title_todo.length < 4) || (title_todo.value = "") ){
+      //requiredField(title_todo, "required");
+    } else if ( (description_todo.length < 4) || (description_todo.value = "") ){
+      //requiredField(description_todo, "required");
     } else {
-
+      //requiredField(title_todo, "required");
+      //requiredField(description_todo, "required");
+      myApp.addToDo(title_todo, description_todo);
+      title_todo.value = "";
+      description_todo.value = "";
     }
-
-    console.log(myApp.getToDoList());
-
+    myApp.showToDoList();
   });
 
 
-  // var obj = myApp.getToDoList();
-  // obj.forEach(function (k){
-  //   li = document.createElement("li");
-  //   li_title = k.title;
-  //   li_description = k.description;
-  //   li_year = k.date[2];
-  //   li_content = "<div class='todo_item'><span class='title'><h2>"+li_title+"</h2></span><span class='date'>"+li_year+"</span><span class='description'><p>"+li_description+"</p></span></div>";
-  //   li.innerHTML = li_content;
-  //   ul_list.appendChild(li);
-  // });
-  //
-  // console.log(myApp.getToDoList());
+
 
 
 });
